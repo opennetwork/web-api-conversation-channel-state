2583a6085ec33e34f0c484d35a7da4aa7bd28649
Fabian Cook <hello@fabiancook.dev>
2019-08-20 19:25:19 +1200

    Typed access modes

--- src/comment/type.ts
+++ src/comment/type.ts
@@ -2,8 +2,17 @@ import { WebAPIObject } from "../object";
 import { WebAPIPerson } from "../person";
 import { ImageObject, AudioObject, VideoObject, Text } from "schema-dts";
 import { Type } from "../type";
-// We can infer typing's if we wanted to... this doesn't belong here though
-// ["auditory", "visual"] | ["auditory"] | ["textual", "visual"] | ["textual", "auditory", "visual"]
+
+export type AccessModeVisual = "visual";
+export type AccessModeTextual = "textual";
+export type AccessModeAuditory = "auditory";
+export type AccessMode = AccessModeVisual | AccessModeTextual | AccessModeAuditory;
+
+export type AccessModeVideo = [AccessModeVisual, AccessModeAuditory];
+export type AccessModeVideoCaptioned = [AccessModeVisual, AccessModeAuditory, AccessModeTextual];
+export type AccessModeImage = [AccessModeVisual];
+export type AccessModeImageCaptioned = [AccessModeVisual, AccessModeTextual];
+
 export interface WebAPICommentMediaBase extends WebAPIObject {
   // Its either going to be one of:
   // - One of visual, textual, auditory
@@ -11,7 +20,7 @@ export interface WebAPICommentMediaBase extends WebAPIObject {
   // - textual & visual, aka image with alt
   // - textual & auditory & visual, aka video with sound and captions
   creator: WebAPIPerson;
-  accessMode: ("visual" | "textual" | "auditory")[];
+  accessMode: AccessMode[];
   text?: Text;
   image?: ImageObject;
   audio?: AudioObject;
